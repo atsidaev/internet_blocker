f21d7a20f821902d9e0bc9a8efeb641a89afe6be
Alexander Tsidaev <a.tsidaev@gmail.com>
2017-07-02 02:06:32 +0500

    Rename confusing parameters - now 'inet.sh disable' disables internet

--- inet.sh
+++ inet.sh
@@ -3,7 +3,7 @@
 rule_nat="-p tcp -j DNAT --to-destination=192.168.1.1:8088"
 rule_fw="-p tcp --dport=443 -j DROP"
 
-if [ "$1" == "disable" ]
+if [ "$1" == "enable" ]
 then
 	while iptables -t nat -D PREROUTING $rule_nat; do echo; done
 	while iptables -D FORWARD $rule_fw; do echo; done
